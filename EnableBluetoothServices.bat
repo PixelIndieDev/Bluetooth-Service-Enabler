7da0634492b862cfc0cd94654631855a548e1742
Pixel <89741457+PixelIndieDev@users.noreply.github.com>
2026-01-30 13:59:24 +0100

    Add files via upload

---
+++ EnableBluetoothServices.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+:: Check admin permissions, otherwise you can't start any services
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    	echo Administrative permissions confirmed...
+) else (
+    	echo ----------------------------------------------------
+   	echo ERROR: Please right-click and "Run as Administrator"
+    	echo ----------------------------------------------------
+    	pause
+    	exit
+)
+
+echo Starting Bluetooth Services...
+
+:: Start the static named Bluetooth services
+net start bthserv /y
+net start BTAGService /y
+
+:: Start the dynamicly named BluetoothUserService
+for /f "tokens=*" %%S in ('reg query HKLM\SYSTEM\CurrentControlSet\Services ^| findstr "BluetoothUserService_"') do (
+    	set SERVICE=%%~nxS
+	echo Found service: !SERVICE!
+	net start "!SERVICE!" /y
+)
+
+echo.
+echo Done! Your Bluetooth services should be running.
+
+exit
